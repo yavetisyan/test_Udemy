ddcdadebf77e8fde41eeefeb4bece842ead7eccd
Yura <y.avetisyan8@gmail,com>
2020-12-08 16:23:06 +0400

    This is my first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+let arr = ['plum.png', 'orange.jpg', 'appl.bmp'];
+
+
+//let answer = +prompt('Are you 18?', 'Yes');
+
+/* console.log(typeof (answer));
+console.log(typeof ('arr' + ' - object'));
+console.log(typeof (4 + +' - object'));
+
+
+ */
+
+ 
+
