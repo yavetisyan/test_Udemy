9a295fb75c496daaa97eeaf5cf041bc172348877
Yura <y.avetisyan88@gmail.com>
2020-12-12 23:22:19 +0400

    Function js

---
+++ js/cikli.js
@@ -0,0 +1,21 @@
+let num = 50;
+
+/* while (num < 55) {
+    console.log(num);
+
+    num++;
+}; */
+
+/* do {
+    console.log(num);
+    num++;
+} while (num < 55); */
+/* 
+for (let i = 0; i < 8; i++) {
+    if (i == 6) {
+        continue
+    }
+    console.log(i);
+} */
+
+
---
+++ js/func.js
@@ -0,0 +1,24 @@
+/* function showFirstMeessage(text) {
+    console.log(text);
+
+};
+
+showFirstMeessage('Heloo');
+
+ */
+
+/* function calc(a, b) {
+    return (a + b);
+} */
+
+let calc = (a, b) => a + b;
+
+console.log(calc(5, 6));
+console.log(calc(3, 1));
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+let anotherNum = retVar();
+console.log(anotherNum);
